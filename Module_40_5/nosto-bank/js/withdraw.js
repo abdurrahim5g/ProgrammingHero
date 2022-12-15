3b6c318dc8daa4e92a9c37175305a578ab9136e3
f-rahim <freelancerrahim26@gmail.com>
2022-12-15 16:26:13 +0600

    Complete Bap ar bank Debugging and Fixing

--- Module_40_5/nosto-bank/js/withdraw.js
+++ Module_40_5/nosto-bank/js/withdraw.js
@@ -11,10 +11,14 @@
 
 document.getElementById("btn-withdraw").addEventListener("click", function () {
   const newWithdrawAmount = getInputFieldValueById("withdraw-field");
-  const previousWithdrawTotal = getTextElementValueById("withdraw-total ");
+  const previousWithdrawTotal = getTextElementValueById("withdraw-total");
   const newWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
-  setTextElementValueById(withdraw - total, newWithdrawTotal);
   const previousBalanceTotal = getTextElementValueById("balance-total");
-  const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
-  setTextElementValueById("balance-total", newBalanceTotal);
+  if (newWithdrawAmount < previousBalanceTotal) {
+    setTextElementValueById("withdraw-total", newWithdrawTotal);
+    const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
+    setTextElementValueById("balance-total", newBalanceTotal);
+  } else {
+    alert(`You don't hvae enough balance.`);
+  }
 });
